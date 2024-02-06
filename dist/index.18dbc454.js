d501be7f1d19153977801b94e713cf2f4ebe0e67
Anton <erikssonanton94@gmail.com>
2024-02-06 09:46:03 +0100

    Körde npm run start och löste problemet med bildredigeringen genom parcel. Yay!

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -579,6 +579,21 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
+"use strict";
+//Häntar knapparna för mobilmenyn
+let openBtn = document.getElementById("open-menu");
+let closeBtn = document.getElementById("close-menu");
+let languageOpenBtn = document.getElementById("language-menu");
+let languageCloseBtn = document.getElementById("language-close");
+// Ger knapparna sina funktioner att öppna och stänga.
+openBtn.addEventListener("click", toggleMenu);
+closeBtn.addEventListener("click", toggleMenu);
+function toggleMenu() {
+    let mySidenavEl = document.getElementById("mySidenav");
+    let style = window.getComputedStyle(mySidenavEl);
+    if (style.display === "none") mySidenavEl.style.display = "block";
+    else mySidenavEl.style.display = "none";
+}
 
 },{}]},["iqNlW","1SICI"], "1SICI", "parcelRequireeb40")
 
