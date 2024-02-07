a50ce0978f2d8d650191633a5c2fc710f94f632a
Anton <erikssonanton94@gmail.com>
2024-02-07 09:46:01 +0100

    Testar uppdatering då sidan inte hittar mitt ramschema

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -594,6 +594,75 @@ function toggleMenu() {
     if (style.display === "none") mySidenavEl.style.display = "block";
     else mySidenavEl.style.display = "none";
 }
+//UPPGIFT 2!
+const url = "../ramschema_ht23.json";
+window.onload = init();
+let kurser = [];
+const kurserEl = document.getElementById("kurserTabell");
+//Kopplar sökrutan till event listener
+//Event listenern jämför texten i sökrutan mot texterna i tabellen, och visar dom som matchar.
+const searchbarEl = document.getElementById("searchbar");
+searchbarEl.addEventListener("keyup", (e)=>{
+    const searchString = e.target.value.toLowerCase();
+    const filteredSearch = kurser.filter((kurs)=>{
+        return kurs.code.toLowerCase().includes(searchString) || kurs.coursename.toLowerCase().includes(searchString);
+    });
+    kurserEl.innerHTML = "";
+    displayCourses(filteredSearch);
+});
+async function init() {
+    try {
+        //Fetch-anrop
+        const response = await fetch(url);
+        kurser = await response.json();
+        //Hämtar in den tomma tabellen
+        const kurserEl = document.getElementById("kurserTabell");
+        //Sorteringar via event handlers
+        const kodEl = document.getElementById("kurskod");
+        kodEl.addEventListener("click", sortByCode);
+        const namnEl = document.getElementById("kursnamn");
+        namnEl.addEventListener("click", sortByName);
+        const progressionEl = document.getElementById("progression");
+        progressionEl.addEventListener("click", sortByProgress);
+        //Funktion för att sortera på kurskod
+        function sortByCode() {
+            kurser.sort((a, b)=>a.code > b.code ? 1 : -1);
+            kurserEl.innerHTML = "";
+            displayCourses(kurser);
+        }
+        //Funktion för att sortera på kursnamn
+        function sortByName() {
+            kurser.sort((a, b)=>a.coursename > b.coursename ? 1 : -1);
+            kurserEl.innerHTML = "";
+            displayCourses(kurser);
+        }
+        //Funktion för att sortera på progression
+        function sortByProgress() {
+            kurser.sort((a, b)=>a.progression > b.progression ? 1 : -1);
+            kurserEl.innerHTML = "";
+            displayCourses(kurser);
+        }
+        //Loggar för kontroll
+        console.table(kurser);
+        displayCourses(kurser);
+    } catch  {
+        //Felmeddelande om något inte stämmer
+        document.getElementById("error").innerHTML = "<p>N\xe5got gick fel...</p>";
+    }
+}
+function displayCourses(kurser) {
+    //Hämtar in den tomma tabellen
+    const kurserEl = document.getElementById("kurserTabell");
+    //Lägger in alla kurser i den tomma tabellen genom en forEach loop
+    kurser.forEach((kurs)=>{
+        kurserEl.innerHTML += `
+        <tr>
+            <td>${kurs.code}</td>
+            <td>${kurs.coursename}</td>
+            <td>${kurs.progression}</td>
+        </tr>`;
+    });
+}
 
 },{}]},["iqNlW","1SICI"], "1SICI", "parcelRequireeb40")
 
